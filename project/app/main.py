3caa08ca637aa60b56b07d0ec965f7b111e645bb
Hala <hqadi@uwaterloo.ca>
2026-07-27 20:31:11 -0400

    Refactoring

--- project/app/main.py
+++ project/app/main.py
@@ -1,23 +1,15 @@
-import os
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Depends
-from app.config import get_settings, Settings
-from tortoise.contrib.fastapi import register_tortoise
+from app.api import ping
+from app.db import init_db
 
-app = FastAPI()
 
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+    return application
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+
+app = create_application()
+
+init_db(app)
