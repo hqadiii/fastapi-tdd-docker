bed36788ccabe6661b9de775ace5550fff59da5b
Hala <hqadi@uwaterloo.ca>
2026-07-29 10:17:33 -0400

    Basic routes

---
+++ project/app/api/crud.py
@@ -0,0 +1,18 @@
+from app.models.pydantic import SummaryPayloadSchema
+from app.models.tortoise import TextSummary
+from typing import Union, List
+
+async def post(payload: SummaryPayloadSchema) -> int:
+    summary = TextSummary(url=payload.url, summary="dummy summary")
+
+    await summary.save()
+    return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    return summary if summary else None
+
+
+async def get_all() -> List:
+    return await TextSummary.all().values()
---
+++ project/app/api/summaries.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, HTTPException
+from typing import List
+from app.models.pydantic import SummaryResponseSchema, SummaryPayloadSchema
+from app.api import crud
+from app.models.tortoise import SummarySchema
+
+router = APIRouter()
+
+
+@router.post("/", response_model=SummaryResponseSchema, status_code=201)
+async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
+    id = await crud.post(payload)
+    return {"id": id, "url": payload.url}
+
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def get_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+            raise HTTPException(status_code=404, detail="Summary not found")
+    return summary
+
+
+@router.get("/", response_model=List[SummarySchema])
+async def get_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
