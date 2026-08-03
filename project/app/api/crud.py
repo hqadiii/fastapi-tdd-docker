63724776573e73ffa90e78716947ec8f1ea47bf3
Hala <hqadi@uwaterloo.ca>
2026-08-03 11:33:52 -0400

    Add routes for delete and update summary

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,7 +1,11 @@
 from typing import List, Union
 
-from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+
+from app.models.pydantic import (  # isort:skip
+    SummaryPayloadSchema,
+    SummaryUpdatePayloadSchema,
+)
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -18,3 +22,17 @@ async def get(id: int) -> Union[dict, None]:
 
 async def get_all() -> List:
     return await TextSummary.all().values()
+
+
+async def delete(id: int) -> int:
+    return await TextSummary.filter(id=id).delete()
+
+
+async def put(id: int, payload: SummaryUpdatePayloadSchema) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary
+    return None
--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -49,3 +49,80 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
+
+
+def test_delete_summary(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
+    assert response.status_code == 204
+
+
+def test_delete_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    summary_id = response.json()["id"]
+    payload = {"url": "https://foo.bar", "summary": "updated!"}
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", json=payload)
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == payload["url"]
+    assert response_dict["summary"] == payload["summary"]
+    assert response_dict["created_at"]
+
+
+def test_update_summary_incorrect_id(test_app_with_db):
+    payload = {"url": "https://foo.bar", "summary": "updated!"}
+    response = test_app_with_db.put("/summaries/999/", json=payload)
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary_invalid_json(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", json={})
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "input": {},
+                "loc": ["body", "url"],
+                "msg": "Field required",
+                "type": "missing",
+            },
+            {
+                "input": {},
+                "loc": ["body", "summary"],
+                "msg": "Field required",
+                "type": "missing",
+            },
+        ]
+    }
+
+
+def test_update_summary_invalid_keys(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    summary_id = response.json()["id"]
+    payload = {"url": "https://foo.bar"}
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", json=payload)
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "input": {"url": "https://foo.bar"},
+                "loc": ["body", "summary"],
+                "msg": "Field required",
+                "type": "missing",
+            }
+        ]
+    }
