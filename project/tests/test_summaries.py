f5b96844a6feb1ad9963e96cb1361f14fa94b628
Hala <hqadi@uwaterloo.ca>
2026-08-03 19:50:29 -0400

    Add URL validation

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -2,10 +2,10 @@ from app.models.tortoise import TextSummary
 
 
 def test_create_summary(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar/"})
 
     assert response.status_code == 201
-    assert response.json()["url"] == "https://foo.bar"
+    assert response.json()["url"] == "https://foo.bar/"
 
 
 def test_create_summaries_invalid_json(test_app):
@@ -22,9 +22,15 @@ def test_create_summaries_invalid_json(test_app):
         ]
     }
 
+    response = test_app.post("/summaries/", json={"url": "invalid://url"})
+    assert response.status_code == 422
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    )
+
 
 def test_read_summary(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar/"})
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get(f"/summaries/{summary_id}/")
@@ -32,7 +38,7 @@ def test_read_summary(test_app_with_db):
 
     response_dict = response.json()
     assert response_dict["id"] == summary_id
-    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["url"] == "https://foo.bar/"
     assert response_dict["summary"]
     assert response_dict["created_at"]
 
@@ -59,7 +65,7 @@ def test_read_summary_incorrect_id(test_app_with_db):
 
 
 def test_read_all_summaries(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar/"})
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get("/summaries/")
@@ -70,7 +76,7 @@ def test_read_all_summaries(test_app_with_db):
 
 
 def test_delete_summary(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar/"})
     summary_id = response.json()["id"]
 
     response = test_app_with_db.delete(f"/summaries/{summary_id}/")
@@ -99,9 +105,9 @@ def test_delete_summary_incorrect_id(test_app_with_db):
 
 
 def test_update_summary(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar/"})
     summary_id = response.json()["id"]
-    payload = {"url": "https://foo.bar", "summary": "updated!"}
+    payload = {"url": "https://foo.bar/", "summary": "updated!"}
     response = test_app_with_db.put(f"/summaries/{summary_id}/", json=payload)
     assert response.status_code == 200
 
@@ -114,7 +120,7 @@ def test_update_summary(test_app_with_db):
 
 def test_update_summary_incorrect_id(test_app_with_db):
     test_app_with_db.portal.call(TextSummary.all().delete)
-    payload = {"url": "https://foo.bar", "summary": "updated!"}
+    payload = {"url": "https://foo.bar/", "summary": "updated!"}
     response = test_app_with_db.put("/summaries/1/", json=payload)
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
@@ -135,7 +141,7 @@ def test_update_summary_incorrect_id(test_app_with_db):
 
 
 def test_update_summary_invalid_json(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar/"})
     summary_id = response.json()["id"]
 
     response = test_app_with_db.put(f"/summaries/{summary_id}/", json={})
@@ -159,18 +165,27 @@ def test_update_summary_invalid_json(test_app_with_db):
 
 
 def test_update_summary_invalid_keys(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar"})
+    response = test_app_with_db.post("/summaries/", json={"url": "https://foo.bar/"})
     summary_id = response.json()["id"]
-    payload = {"url": "https://foo.bar"}
+    payload = {"url": "https://foo.bar/"}
     response = test_app_with_db.put(f"/summaries/{summary_id}/", json=payload)
     assert response.status_code == 422
     assert response.json() == {
         "detail": [
             {
-                "input": {"url": "https://foo.bar"},
+                "input": {"url": "https://foo.bar/"},
                 "loc": ["body", "summary"],
                 "msg": "Field required",
                 "type": "missing",
             }
         ]
     }
+
+    payload_with_invalid_url = {"url": "invalid://url", "summary": "updated!"}
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/", json=payload_with_invalid_url
+    )
+    assert response.status_code == 422
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    )
